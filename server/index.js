d49e942573e1f220af38368422f3dcaeed0bd49f
Aman Goswami <amangoswami2k3@gmail.com>
2023-08-02 10:23:08 +0530

    Server ready

--- server/index.js
+++ server/index.js
@@ -1,33 +1,34 @@
-const express = require("express")
-const app = express()
-app.use(express.json())
-const mongoose = require("mongoose")
+const express = require("express");
+const app = express();
+app.use(express.json());
+const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const { connectDB } = require("./databases/database");
-const  cors = require("cors")
-const userRoutes = require("./routes/userRoutes")
-const policeRoutes = require("./routes/policeRoutes")
+const cors = require("cors");
+const userRoutes = require("./routes/userRoutes");
+const policeRoutes = require("./routes/policeRoutes");
 dotenv.config();
-app.use(cors({
-    "origin": "*",
+app.use(
+  cors({
+    origin: "*",
     methods: "GET,POST,PUT,DELETE,PATCH",
     credentials: true,
-}))
-// app.get('/',(req,res)=>{
-//     res.send("The world is a stage where every man must play a part!")
-// })
+  })
+);
 
-app.use('/api/police',policeRoutes)
-app.use('/api/users',userRoutes)
+app.use("/api/police", policeRoutes);
+app.use("/api/users", userRoutes);
 
 // serving frontend
-const __dirname = path.resolve();
-app.use(express.static(path.join(__dirname, '../crime/build')));
-console.log(path.join(__dirname, '../crime/build'))
-app.get("*",function(req,res){
-  res.sendFile(path.join(__dirname, '../crime/build', 'index.html'));
+const path = require("path");
+const buildPath = path.join(__dirname, "../crime/build");
+app.use(express.static(buildPath));
+
+app.get("*", function (req, res) {
+  res.sendFile(path.join(buildPath, "index.html"));
 });
+
 app.listen(5000, () => {
-    console.log("Backend Server is running on port 5000")
-})
+  console.log("Backend Server is running on port 5000");
+});
 connectDB();
